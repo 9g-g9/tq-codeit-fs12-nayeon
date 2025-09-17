f565c35a204039c3673a44e8735e760f2fbc29e0
wonee09 <wonjang031@gmail.com>
2025-09-17 16:20:38 +0900

    first commit

---
+++ src/app/_components/TodoList.js
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import TodoItem from "@/app/_components/TodoItem";
+import { fetchTodos } from "@/api/todos";
+import { useQuery } from "@tanstack/react-query";
+
+export default function TodoList() {
+  const {
+    data: todos,
+    isPending,
+    error,
+  } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
+
+  if (isPending)
+    return (
+      <div className="container mx-auto px-4 py-8 text-center">로딩 중...</div>
+    );
+
+  if (error)
+    return (
+      <div className="container mx-auto px-4 py-8 text-center text-red-500">
+        {error}
+      </div>
+    );
+  return (
+    <div className="border">
+      {todos.length === 0 ? (
+        <div className="p-4 text-center">할 일이 없습니다.</div>
+      ) : (
+        todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+      )}
+    </div>
+  );
+}
