119a6f159f1e70b914fea0b8d9d259aabf2b2ff5
9g-g9 <nayun12375@gmail.com>
2026-05-11 14:14:38 +0900

    실습 완료

--- src/app/_components/TodoForm.js
+++ src/app/_components/TodoForm.js
@@ -1,34 +1,44 @@
 "use client";
 
 import { addTodo } from "@/api/todos";
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
 export default function TodoForm() {
   const [title, setTitle] = useState("");
 
   const queryClient = useQueryClient();
-
-  const handleAddTodo = async (title) => {
-    try {
+  const postTodoMutation = useMutation({
+    mutationFn: async ({ title }) => {
       await addTodo(title);
       await queryClient.invalidateQueries({ queryKey: ["todos"] });
-    } catch (err) {
-      console.error("할 일 추가 중 오류 발생:", err);
-      setError("할 일을 추가하는데 실패했습니다.");
-    }
-  };
+    },
+  });
+
+  // const handleAddTodo = async (title) => {
+  //   try {
+  //     await addTodo(title);
+  //     await queryClient.invalidateQueries({ queryKey: ["todos"] });
+  //   } catch (err) {
+  //     console.error("할 일 추가 중 오류 발생:", err);
+  //     setError("할 일을 추가하는데 실패했습니다.");
+  //   }
+  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!title.trim()) return;
 
-    try {
-      await handleAddTodo(title);
-      setTitle(""); // 입력 필드 초기화
-    } catch (err) {
-      console.error("할 일 추가 중 오류 발생:", err);
-    }
+    postTodoMutation.mutate({
+      title,
+    });
+
+    // try {
+    //   await handleAddTodo(title);
+    //   setTitle(""); // 입력 필드 초기화
+    // } catch (err) {
+    //   console.error("할 일 추가 중 오류 발생:", err);
+    // }
   };
 
   return (
